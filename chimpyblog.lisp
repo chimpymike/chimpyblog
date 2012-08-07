96a159adc26b587a6c1d4ddb89fa3a07d68f3d8f
Mike Callahan <chimpymike@gmail.com>
2012-08-07 15:41:55 -0400

    Add database utility functions

--- chimpyblog.lisp
+++ chimpyblog.lisp
@@ -2,5 +2,16 @@
 
 (in-package #:chimpyblog)
 
-;;; "chimpyblog" goes here. Hacks and glory await!
+;;;; Database utilities
+(defun db-create (host db-name username password)
+  (create-database (list host db-name username password) :database-type :postgresql))
+
+(defun db-connect (host db-name username password)
+  (connect (list host db-name username password) :database-type :postgresql))
+
+(defun create-tables ()
+  (mapcar #'create-view-from-class '(post)))
+
+(defun drop-tables ()
+  (mapcar #'drop-view-from-class '(post)))
 
