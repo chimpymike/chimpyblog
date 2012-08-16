d9264b13a4194ef29e5d54e40b578aebc3531834
Mike Callahan <chimpymike@gmail.com>
2012-08-16 16:12:03 -0400

    Add view for the home page in the default theme.

--- themes/default.lisp
+++ themes/default.lisp
@@ -1,1 +1,10 @@
 (in-package #:chimpyblog)
+
+(defun home-page-view (posts)
+  (with-html-output-to-string (*standard-output* nil :prologue t :indent t)
+    (:html
+     (:body
+      (:h1 "Posts")
+      (:h2 (:a :href "/add-post" "Add a new post"))
+      (:div
+       (mapcar #'perma-link posts))))))
