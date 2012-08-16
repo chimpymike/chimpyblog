23fb5f654f1282bcbf39f6f714b7694884d1fc79
Mike Callahan <chimpymike@gmail.com>
2012-08-16 16:07:21 -0400

    Add handler to show the single post page.

--- handlers.lisp
+++ handlers.lisp
@@ -1,1 +1,17 @@
 (in-package #:chimpyblog)
+
+(defun get-slug-from-uri (uri)
+  (string-trim '(#\/) uri))
+
+(defun show-post ()
+  (let* ((requested-post-slug (get-slug-from-uri (request-uri*)))
+	 (requested-post (get-post-by-slug requested-post-slug)))
+    (if requested-post
+	(progn
+	  (with-slots (title body) requested-post
+	    (with-html-output-to-string (*standard-output* nil :prologue t :indent t)
+	      (:html
+	       (:body
+		(:h1 (str title))
+		(:div :class "body" (str body)))))))
+	nil)))
